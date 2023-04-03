309d3b90cd184003b939a82f4dccafb84d727d78
Yuri Kolesnikov <>
2023-04-03 19:08:25 +0300

    some

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,39 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { outputFileSync, readJsonSync, readdirSync } from 'fs-extra';
 // import kebabCase from "lodash/kebabCase";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const DEFAULT_LOCALE = 'en-EN';
+  let basePath;
+  const folders = vscode?.workspace?.workspaceFolders;
+
+  if (folders && folders[0]) {
+    basePath = folders[0].uri.path + '/src/locales';
+  }
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "codetranslate" is now active!');
 
 	context.subscriptions.push(vscode.commands.registerCommand('CodeTranslate.addKey', async () => {
+    if (!basePath) {
+      vscode.window.showErrorMessage(`Something went wrong!`);
+
+      return;
+    }
+
     const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
     const selecetedText = editor?.selection;
     const valueSelectedText = editor?.document.getText(selecetedText);
 
-    let key = '';
-    let value = '';
-    let resOfInputBoxKey = null;
-
     let inputBoxOptionsKey: vscode.InputBoxOptions = {
       prompt: "Key:",
-      placeHolder: "Enter key"
+      placeHolder: "Enter key",
+      value: valueSelectedText
     };
 
     let inputBoxOptionsValue: vscode.InputBoxOptions = {
@@ -30,42 +41,36 @@ export function activate(context: vscode.ExtensionContext) {
       placeHolder: "Enter value"
     };
 
-    if (!valueSelectedText) {
-      resOfInputBoxKey = await vscode.window.showInputBox(inputBoxOptionsKey);
-    }
-
+    const resOfInputBoxKey = await vscode.window.showInputBox(inputBoxOptionsKey);
     const resOfInputBoxValue = await vscode.window.showInputBox(inputBoxOptionsValue);
 
-    console.log('resOfInputBoxKey', resOfInputBoxKey);
-    console.log('resOfInputBoxValue', resOfInputBoxValue);
+    const key = resOfInputBoxKey;
 
-    // vscode.window.showInputBox(options).then((value) => {
-    //   if (!value) return
+    const pathArray = key.split(".");
 
-    //   answer1 = value;
+      const fileJson = readJsonSync(`${basePath}/${DEFAULT_LOCALE}/${pathArray.slice(0, pathArray.length - 1).join('/')}.json`);
+      // If key already exists
+      if (fileJson && fileJson.hasOwnProperty(pathArray[pathArray.length - 1])) {
+        vscode.window.showErrorMessage(`Key "${key}" already exists!`);
 
-    //   const path = {
-    //     common: '',
-    //     pages: '',
-    //     components: '',
-    //   };
+        return;
+      }
 
-    //   console.log('value1', value);
-    // });
+      const langs = readdirSync(basePath);
 
-    // vscode.window.showInputBox(options2).then((value) => {
-    //   if (!value) return
+      langs.forEach(locale => {
+        const localFileJson = readJsonSync(`${basePath}/${locale}/${pathArray.slice(0, pathArray.length - 1).join('/')}.json`);
 
-    //   answer2 = value;
+        if (localFileJson) {
+          const res = {...localFileJson, [pathArray[pathArray.length - 1]]: locale === DEFAULT_LOCALE ? resOfInputBoxValue : ''};
 
-    //   const path = {
-    //     common: '',
-    //     pages: '',
-    //     components: '',
-    //   };
+          outputFileSync(`${basePath}/${locale}/${pathArray.slice(0, pathArray.length - 1).join('/')}.json`, JSON.stringify(res, null, 2));
+        } else {
+          outputFileSync(`${basePath}/${locale}/${pathArray.slice(0, pathArray.length - 1).join('/')}.json`, JSON.stringify({}, null, 2));
+        }
+      });
 
-    //   console.log('valu2', value);
-    // });
+      vscode.window.showInformationMessage(`Key with name "${key}" has been success added!`);
 	}));
 }
 
